62afd2bc395c28a6dd7b7b4eb9d69dcc22323c86
Hilary Onianwa <hilaryebubechukwu1@gmail.com>
2025-09-05 11:37:40 +0100

    initial commit

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,7 +2,7 @@ import { Heading } from "@chakra-ui/react";
 import React from "react";
 
 const AboutUs = () => {
-  return <Heading>About Us page</Heading>;
+  return <Heading textAlign="center">About Us page</Heading>;
 };
 
 export default AboutUs;
